c4995419f78c84e10bc26c75286c1800cc03a7fe
Henry Nguyen <acc.tr100@gmail.com>
2024-01-23 01:44:14 -0500

    PC

---
+++ js/login.js
@@ -0,0 +1,21 @@
+function clickLogin() {
+    var log = document.getElementById("login");
+    var reg = document.getElementById("signup");
+    var but = document.getElementById("btn");
+
+    log.style.left = "-400px";
+    reg.style.left = "0px";
+    but.style.left = "130px";
+}
+
+function clickSignUp() {
+
+    var log = document.getElementById("login");
+    var reg = document.getElementById("signup");
+    var but = document.getElementById("btn");
+
+    reg.style.left = "-400px";
+    log.style.left = "0px";
+    but.style.left = "0px";
+
+}
