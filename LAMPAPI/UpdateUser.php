8f8f5e16ac0618008c068ddfafbcee77fefe733a
Henry Nguyen <acc.tr100@gmail.com>
2024-02-14 00:28:27 -0500

    PC

--- LAMPAPI/UpdateUser.php
+++ LAMPAPI/UpdateUser.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	$inData = getRequestInfo();
 	
@@ -40,46 +39,3 @@
 		sendResultInfoAsJson( $retValue );
 	}
 ?>
-=======
-<?php
-	$inData = getRequestInfo();
-	
-	$userId = $inData["userId"];
-	$firstName = $inData["firstName"];
-	$lastName = $inData["lastName"];
-	$login = $inData["login"];
-	$password = $inData["password"];
-
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("UPDATE Users SET FirstName=?, LastName=?, Login=?, Password=? WHERE ID=?");
-		$stmt->bind_param("sssss", $firstName, $lastName, $login, $password, $userId);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
-
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-?>
->>>>>>> c4f49488e1f96e9a3e3744735f942085cf664221
